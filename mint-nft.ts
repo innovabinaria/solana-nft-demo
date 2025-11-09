73cd35b40b98f29badbfc9636172c79b8dff0b14
Victor Aguayo <vaguayo@gmail.com>
2025-11-09 02:45:56 -0500

    Initial commit - Solana NFT demo (Metaplex Umi)

---
+++ mint-nft.ts
@@ -0,0 +1,47 @@
+import { readFileSync } from "fs";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import {
+  createSignerFromKeypair,
+  generateSigner,
+  signerIdentity,
+  percentAmount,
+} from "@metaplex-foundation/umi";
+import {
+  mplTokenMetadata,
+  createNft,
+} from "@metaplex-foundation/mpl-token-metadata";
+
+async function main() {
+  const umi = createUmi("https://api.devnet.solana.com").use(mplTokenMetadata());
+
+  const walletPath = `${process.env.HOME}/.config/solana/id.json`;
+  const secretKey = Uint8Array.from(JSON.parse(readFileSync(walletPath, "utf8")));
+  const keypair = umi.eddsa.createKeypairFromSecretKey(secretKey);
+  const signer = createSignerFromKeypair(umi, keypair);
+  umi.use(signerIdentity(signer));
+
+  console.log("Usando wallet:", signer.publicKey.toString());
+
+  const mint = generateSigner(umi);
+
+  const metadataUri = "https://raw.githubusercontent.com/innovabinaria/solana-nft-assets/main/mi-primer-nft.json";
+
+  const { signature } = await createNft(umi, {
+    mint,
+    name: "Mi Primer NFT en Solana",
+    symbol: "VIK",
+    uri: metadataUri,
+    sellerFeeBasisPoints: percentAmount(5), // 5% royalties
+  }).sendAndConfirm(umi);
+
+  console.log("Tx signature:", signature);
+  console.log("Mint address:", mint.publicKey.toString());
+  console.log(
+    `Explorer (devnet): https://explorer.solana.com/address/${mint.publicKey.toString()}?cluster=devnet`
+  );
+}
+
+main().catch((err) => {
+  console.error("Error al mintear:", err);
+  process.exit(1);
+});
